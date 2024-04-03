ddba4a9eaec02a209e7f60f18457cb9f5e020f63
Andreas Lindner <mail@lindnera.de>
2024-04-03 16:33:51 +0200

    resend device offline state

--- workers/base.py
+++ workers/base.py
@@ -51,7 +51,7 @@ class BaseWorker:
     def avail_offline(self, name):
         self.error_count+= 1
         _LOGGER.info("  Error count for %s is %d (from %d)", name, self.error_count, self.errors_to_offline)
-        if (self.error_count >= self.errors_to_offline and self.is_online is not False):
+        if (self.error_count >= self.errors_to_offline and (self.is_online is not False or self.error_count % 100 == 0)):
             self.is_online = False
             #self.error_count = 0
             return [MqttMessage(topic=self.format_topic(name, "availability"), payload="offline")]
